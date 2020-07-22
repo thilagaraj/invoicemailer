b7fd2f442ca95b28c1a2ef200123c859594a0fc2
Thilagaraj <tk.thilagaraj@gmail.com>
2020-07-22 08:29:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ ini_set('default_socket_timeout',560);
 	include 'invoice-template.php';
 	$mail = new PHPMailer;
 	$mail->isSMTP();
-        //$mail->SMTPDebug = 1;
+        $mail->SMTPDebug = 1;
 	$mail->SMTPAuth = true;
 	$mail->Host = 'sg2plcpnl0108.prod.sin2.secureserver.net';
 	$mail->Port = 587;
